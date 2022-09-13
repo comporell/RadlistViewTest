278fe704e230b28994db08c7140cb8a0c1d593b6
Trevize Daneel <comporell@gmail.com>
2022-09-13 11:12:58 +0300

    init

--- src/app/featured/featured.component.ts
+++ src/app/featured/featured.component.ts
@@ -1,12 +1,26 @@
 import { Component, OnInit } from '@angular/core'
 import { RadSideDrawer } from 'nativescript-ui-sidedrawer'
-import { Application } from '@nativescript/core'
+import { Application, ObservableArray } from '@nativescript/core'
 
 @Component({
   selector: 'Featured',
   templateUrl: './featured.component.html',
 })
 export class FeaturedComponent implements OnInit {
+
+  dataItems: DataItem[] = [
+    { "id": 1, "name": "Item 1", "description": "This is item description.", "title": "This is item Title", "text": "This is item Text",  "image": "This is item Image", "selected": false },
+    { "id": 2, "name": "Item 2", "description": "This is item description.", "title": "This is item Title", "text": "This is item Text",  "image": "This is item Image", "selected": false },
+    { "id": 3, "name": "Item 3", "description": "This is item description.", "title": "This is item Title", "text": "This is item Text",  "image": "This is item Image", "selected": false },
+    { "id": 4, "name": "Item 4", "description": "This is item description.", "title": "This is item Title", "text": "This is item Text",  "image": "This is item Image", "selected": false },
+    { "id": 5, "name": "Item 5", "description": "This is item description.", "title": "This is item Title", "text": "This is item Text",  "image": "This is item Image", "selected": false },
+    { "id": 6, "name": "Item 6", "description": "This is item description.", "title": "This is item Title", "text": "This is item Text",  "image": "This is item Image", "selected": false },
+    { "id": 7, "name": "Item 7", "description": "This is item description.", "title": "This is item Title", "text": "This is item Text",  "image": "This is item Image", "selected": false },
+    { "id": 8, "name": "Item 8", "description": "This is item description.", "title": "This is item Title", "text": "This is item Text",  "image": "This is item Image", "selected": false },
+    { "id": 9, "name": "Item 9", "description": "This is item description.", "title": "This is item Title", "text": "This is item Text",  "image": "This is item Image", "selected": false },
+    { "id": 10, "name": "Item 10", "description": "This is item description.", "title": "This is item Title", "text": "This is item Text",  "image": "This is item Image" , "selected": false},
+];
+
   constructor() {
     // Use the component constructor to inject providers.
   }
@@ -20,3 +34,20 @@ export class FeaturedComponent implements OnInit {
     sideDrawer.showDrawer()
   }
 }
+
+// >> listview-angular-data-item
+export class DataItem {
+  constructor(public id?: number,
+      public name?: string,
+      public description?: string,
+      public title?: string,
+      public text?: string,
+      public image?: string,
+      public selected?: boolean,
+      public type?: string,
+      public category?: string,
+      public size?: number,
+      public color?: string) {
+  }
+}
+// << listview-angular-data-item
--- src/app/featured/featured.module.ts
+++ src/app/featured/featured.module.ts
@@ -1,11 +1,13 @@
 import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core'
 import { NativeScriptCommonModule } from '@nativescript/angular'
-
+import { NativeScriptUIListViewModule } from "nativescript-ui-listview/angular";
 import { FeaturedRoutingModule } from './featured-routing.module'
 import { FeaturedComponent } from './featured.component'
 
 @NgModule({
-  imports: [NativeScriptCommonModule, FeaturedRoutingModule],
+  imports: [NativeScriptCommonModule, 
+    NativeScriptUIListViewModule,
+    FeaturedRoutingModule],
   declarations: [FeaturedComponent],
   schemas: [NO_ERRORS_SCHEMA],
 })
